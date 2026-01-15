175cc8fec084521661cc6e5f520cc32598f7f4e3
gargomoma <gargomoma@users.noreply.github.com>
2026-01-15 23:14:35 +0100

    sx126x.cpp - RadioLib wrapper implementation - initial commit

--- sx126x.h
+++ sx126x.h
@@ -1,143 +1,111 @@
-// Copyright Sandeep Mistry, Mark Qvist and Jacob Eva.
-// Licensed under the MIT license.
-
+// sx126x.h - RadioLib wrapper for RNode firmware
 #ifndef SX126X_H
 #define SX126X_H
 
 #include <Arduino.h>
 #include <SPI.h>
-#include "Modem.h"
+#include <RadioLib.h>
 
-#define LORA_DEFAULT_SS_PIN    10
-#define LORA_DEFAULT_RESET_PIN 9
-#define LORA_DEFAULT_DIO0_PIN  2
-#define LORA_DEFAULT_RXEN_PIN  -1
-#define LORA_DEFAULT_TXEN_PIN  -1
-#define LORA_DEFAULT_BUSY_PIN  -1
-#define LORA_MODEM_TIMEOUT_MS 20E3
+#define MODE_STDBY_RC_6X 0x00
+#define MODE_STDBY_XOSC_6X 0x01
+#define MODE_FALLBACK_STDBY_RC_6X 0x20
 
-#define PA_OUTPUT_RFO_PIN      0
-#define PA_OUTPUT_PA_BOOST_PIN 1
+#define IRQ_RX_DONE_MASK_6X 0x40
+#define IRQ_PREAMBLE_DET_MASK_6X 0x10
 
-#define RSSI_OFFSET 157
+#define SYNC_WORD_6X  0x1424
 
-class sx126x : public Stream {
+class sx126x {
 public:
-  sx126x();
+    sx126x(int ss = 10, int rst = 9, int dio0 = 2, int busy = 3, int rxen = -1);
+    ~sx126x();
 
-  int begin(long frequency);
-  void end();
-
-  int beginPacket(int implicitHeader = false);
-  int endPacket();
-
-  int parsePacket(int size = 0);
-  int packetRssi();
-  int packetRssi(uint8_t pkt_snr_raw);
-  int currentRssi();
-  uint8_t packetRssiRaw();
-  uint8_t currentRssiRaw();
-  uint8_t packetSnrRaw();
-  float packetSnr();
-  long packetFrequencyError();
-
-  // from Print
-  virtual size_t write(uint8_t byte);
-  virtual size_t write(const uint8_t *buffer, size_t size);
-
-  // from Stream
-  virtual int available();
-  virtual int read();
-  virtual int peek();
-  virtual void flush();
-
-  void onReceive(void(*callback)(int));
-
-  void receive(int size = 0);
-  void standby();
-  void sleep();
-  void reset(void);
-
-  bool preInit();
-  uint8_t getTxPower();
-  void setTxPower(int level, int outputPin = PA_OUTPUT_PA_BOOST_PIN);
-  uint32_t getFrequency();
-  void setFrequency(long frequency);
-  void setSpreadingFactor(int sf);
-  long getSignalBandwidth();
-  void setSignalBandwidth(long sbw);
-  void setCodingRate4(int denominator);
-  void setPreambleLength(long preamble_symbols);
-  void setSyncWord(uint16_t sw);
-  bool dcd();
-  void enableCrc();
-  void disableCrc();
-  void enableTCXO();
-  void disableTCXO();
-
-  void rxAntEnable();
-  void loraMode();
-  void waitOnBusy();
-  void executeOpcode(uint8_t opcode, uint8_t *buffer, uint8_t size);
-  void executeOpcodeRead(uint8_t opcode, uint8_t *buffer, uint8_t size);
-  void writeBuffer(const uint8_t* buffer, size_t size);
-  void readBuffer(uint8_t* buffer, size_t size);
-  void setPacketParams(long preamble_symbols, uint8_t headermode, uint8_t payload_length, uint8_t crc);
-
-  void setModulationParams(uint8_t sf, uint8_t bw, uint8_t cr, int ldro);
-
-  // deprecated
-  void crc() { enableCrc(); }
-  void noCrc() { disableCrc(); }
-
-  byte random();
-
-  void setPins(int ss = LORA_DEFAULT_SS_PIN, int reset = LORA_DEFAULT_RESET_PIN, int dio0 = LORA_DEFAULT_DIO0_PIN, int busy = LORA_DEFAULT_BUSY_PIN, int rxen = LORA_DEFAULT_RXEN_PIN);
-  void setSPIFrequency(uint32_t frequency);
-
-  void dumpRegisters(Stream& out);
+    bool begin(long frequency);
+    void end();
+    
+    void setFrequency(long frequency);
+    uint32_t getFrequency();
+    
+    void setTxPower(int level);
+    uint8_t getTxPower();
+    
+    void setSpreadingFactor(int sf);
+    void setSignalBandwidth(long sbw);
+    void setCodingRate4(int cr);
+    void enableLowDataRateOptimization();
+    void disableLowDataRateOptimization();
+    void setPreambleLength(long preamble);
+    void setSyncWord(uint16_t sw);
+    void setPacketParams(int crc = 1, int implicitHeader = 0, int payloadLength = 255);
+    void enableCrc();
+    void disableCrc();
+    bool enableTCXO();
+    
+    void calibrate_image(long frequency);
+    void loraMode();
+    void calibrate();
+    
+    void setRxTxFallbackMode(uint8_t mode);
+    
+    int beginPacket(int implicitHeader = 0);
+    int endPacket(bool wait = true);
+    size_t write(uint8_t byte);
+    size_t write(const uint8_t *buffer, size_t size);
+    
+    void onReceive(void(*callback)(int));
+    void receive();
+    
+    void standby();
+    void sleep();
+    void reset();
+    
+    int getRSSI();
+    float getSNR();
+    
+    static void handleDio0Rise();
+    
+    int currentRssi();
+    void startCarrier();
+    bool carrierDetected();
+    void stopCarrier();
+    
+    int available();
+    int read();
+    int peek();
+    void flush();
 
 private:
-  void explicitHeaderMode();
-  void implicitHeaderMode();
-
-  void handleDio0Rise();
-
-  uint8_t readRegister(uint16_t address);
-  void writeRegister(uint16_t address, uint8_t value);
-  uint8_t singleTransfer(uint8_t opcode, uint16_t address, uint8_t value);
-
-  static void onDio0Rise();
-
-  void handleLowDataRate();
-  void optimizeModemSensitivity();
-
-  void calibrate(void);
-  void calibrate_image(long frequency);
-
-private:
-  SPISettings _spiSettings;
-  int _ss;
-  int _reset;
-  int _dio0;
-  int _rxen;
-  int _busy;
-  long _frequency;
-  int _txp;
-  uint8_t _sf;
-  uint8_t _bw;
-  uint8_t _cr;
-  uint8_t _ldro;
-  int _packetIndex;
-  int _preambleLength;
-  int _implicitHeaderMode;
-  int _payloadLength;
-  int _crcMode;
-  int _fifo_tx_addr_ptr;
-  int _fifo_rx_addr_ptr;
-  uint8_t _packet[255];
-  bool _preinit_done;
-  void (*_onReceive)(int);
+    int _ss;
+    int _reset;
+    int _dio0;
+    int _busy;
+    int _rxen;
+    
+    long _frequency;
+    int _txp;
+    uint8_t _sf;
+    uint8_t _bw;
+    uint8_t _cr;
+    uint8_t _ldro;
+    size_t _packetIndex;
+    long _preambleLength;
+    uint8_t _implicitHeaderMode;
+    uint8_t _payloadLength;
+    uint8_t _crcMode;
+    int _fifo_tx_addr_ptr;
+    int _fifo_rx_addr_ptr;
+    
+    uint8_t _packet[256];
+    bool _preinit_done;
+    bool _receiving;
+    bool _tcxo;
+    bool _radio_online;
+    
+    void (*_onReceive)(int);
+    
+    Module* radioModule;
+    SX1262* radio;
+    bool preInit();
 };
 
 extern sx126x sx126x_modem;
